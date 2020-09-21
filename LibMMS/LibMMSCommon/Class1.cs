30802915643d5af461cd764415de69fdb64cbbe0
Anh Tuan Nguyen <tuan.anh.nguyen@plus4u.net>
2020-09-21 11:30:50 +0200

    Zalozeni

---
+++ LibMMS/LibMMSCommon/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibMMSCommon
+{
+  public class Class1
+  {
+    public static string GetMyVersion()
+    {
+      return "MyNuggetVer=1.2.3.1" + "Using IndSoftTestNuget01: " + IndSoftTestNuget01.NugetMain.GetMyVersion();
+    }
+  }
+}
